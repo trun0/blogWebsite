4ad68bf86c368160a2b9ad380d450370c5b93f52
trun0 <tarun2000sbd@gmail.com>
2021-10-26 20:57:08 +0530

    some more changes in server.js

--- frontend/src/components/BaseUrl.js
+++ frontend/src/components/BaseUrl.js
@@ -1,4 +1,4 @@
-const baseURL = "https://blog-website-trun0.herokuapp.com";
-// https://blog-website-trun0.herokuapp.com
+// const baseURL = "https://blog-website-trun0.herokuapp.com";
+const baseURL = "https://localhost:8000";
 
 export default baseURL;
--- server.js
+++ server.js
@@ -185,17 +185,10 @@ app.get("/server", (req, res) => {
 if(process.env.NODE_ENV === 'production') {
 	app.use(express.static('frontend/build'));
 	app.get("*",(req,res) => {
-		res.sendFile(path.resolve(__dirname, "frontend","build","index.html"))
+		res.sendFile(path.resolve(__dirname, "frontend","index.html"))
 	})
 }
 
-app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname, "frontend/public/index.html"), function(err) {
-	  if (err) {
-		res.status(500).send(err)
-	  }
-	})
-  })
 
 app.listen(PORT, () => {
 	console.log("Server started on port " + PORT);
