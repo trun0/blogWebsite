867dd2baf51b1d51252271c965264ae892178650
trun0 <tarun2000sbd@gmail.com>
2021-10-26 22:49:17 +0530

    trying

--- server.js
+++ server.js
@@ -182,16 +182,11 @@ app.get("/server", (req, res) => {
 	res.send({ message: "i have a message" });
 })
 
-// if (process.env.NODE_ENV === 'production') {
-// 	app.use(express.static('frontend/build'));
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-// 	})
-// }
-
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'frontend/build')));
-	app.get('*', (req, res) => { res.sendFile(path.join(__dirname = 'frontend/build/index.html')); })
+	app.use(express.static('frontend/build'));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+	})
 }
 
 app.listen(PORT, () => {
